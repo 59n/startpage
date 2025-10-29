7a26e0b693a53c513892a57ecf5fe30687c11571
59n <51265624+59n@users.noreply.github.com>
2025-10-29 21:36:17 +0100

    Update config

--- config.ts
+++ config.ts
@@ -48,6 +48,9 @@ export const config = {
     { name: 'Calendar', url: 'https://calendar.proton.me', icon: 'calendar' },
     { name: 'Docs', url: 'https://docs.proton.me', icon: 'docs' },
     { name: 'Movies', url: 'http://movieboxpro.app', icon: 'film' },
+    { name: 'NAS', url: 'https://thenas.us', icon: 'x' },
+    { name: 'Portainer', url: 'https://portainer.thenas.us', icon: 'x' },
+    { name: 'Crafty', url: 'https://crafty.thenas.us', icon: 'x' },
   ],
 
   // Link Behavior
